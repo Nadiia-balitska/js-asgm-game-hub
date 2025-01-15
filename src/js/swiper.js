ef0df6c335b4202569fdc3016b08660440bff009
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2025-01-15 16:18:27 +0100

    common settings

---
+++ src/js/swiper.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".horisontal-swiper-container", {
+  direction: "horizontal",
+  loop: true,
+
+  navigation: {
+    nextEl: ".custom-next",
+    prevEl: ".custom-prev",
+  },
+  allowTouchMove: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    1440: {
+      slidesPerView: 2.5,
+    },
+  },
+});
