bdde4e222c8bd15e49bbd011bc53938d557ffcb1
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2025-01-18 23:28:27 +0100

    Add most popular, aboutUs, and footer partials

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,18 +1,30 @@
-const swiper = new Swiper(".horisontal-swiper-container", {
-  direction: "horizontal",
+const swiper = new Swiper('.swiper-container', {
   loop: true,
-
+  autoplay: {
+    delay: 2000,
+  },
+  slidesPerView: 1,
   navigation: {
-    nextEl: ".custom-next",
-    prevEl: ".custom-prev",
+    nextEl: '.custom-next',
+    prevEl: '.custom-prev',
   },
   allowTouchMove: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
   breakpoints: {
-    320: {
-      slidesPerView: 1,
+    425: {
+      slidesPerView: 2,
+    },
+    765: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
     },
     1440: {
-      slidesPerView: 2.5,
+      slidesPerView: 5,
     },
   },
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-import './js/mobile-menu';
+// import './js/mobile-menu';
 import './js/swiper';
-import './js/app-swiper';
+// import './js/app-swiper';
